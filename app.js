aedcc3b21614060901ff36d184260d49d35c2b22
jabir254 <jabirkullow@gmail.com>
2024-04-25 12:13:42 +0300

    server app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
