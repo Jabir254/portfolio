e2858b0468fb2ad5e929985de497b6b4e1f8ba63
jabir254 <jabirkullow@gmail.com>
2024-04-30 11:30:52 +0300

    added views 404 layout

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require("express");
 const app = express();
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
+app.get('/', (req, res) => {
+  res.render('layouts/main')
+})
+
+app.get('/404', (req, res) => {
+  res.render("views/404")
+})
 
 const PORT = process.env.PORT || 3000;
 
