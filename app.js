840dc14d59a907443358676b3872e7d1406f820d
jabir254 <jabirkullow@gmail.com>
2024-05-01 15:02:39 +0300

    added express static file folder

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlers = require("./handlers.js");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use('/static', express.static('public'));
 
 app.get("/", (req, res) => {
   res.render("layouts/index");
